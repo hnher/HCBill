82f5bb5be45836e6b57bf0f791e5864e695acc3d
每一天 <lixing1359199697>
2018-04-09 17:14:20 +0800

    修改了最终样式效果

--- resources/views/home/add.blade.php
+++ resources/views/home/add.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh">
 <head>
 <meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 <title>账单添加</title>
 	<link rel="stylesheet" href="/home/css/exchange.css">
@@ -10,30 +10,75 @@
 	<script  src="/js/layer/layer.js"></script>
 <link rel="stylesheet" type="text/css" href="/css/demo.css">
 <style>
-*{padding:0;margin:0;}
+*{
+	padding:0;
+	margin:0;
+	box-sizing: border-box;
+	}
+html{
+	font-size:62.5%;
+	background: #fafafa;
+	}
 .content {
-	margin:50px auto 0;
-	width:300px;
-	min-height: 500px;
+	width:100%;
+	padding: 0;
 }
-
 .form-group {
 	width:100%;
-	float:left;
-	margin:5px 0;
+	padding: 0 1rem;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	background: #fff;
+    padding-top: .8rem;
+}
+.form-group label{
+	line-height: 4.2rem;
+	font-size: 1.4rem;
+	width: 10rem;
+	text-align: right;
+	border-bottom: 1px solid #aaa;
+	color: #999;
+	margin-right: .4rem;
 }
 
-label{
-	margin-bottom:10px;
-	float:left;			
-}
-
-.field-input, select{
-	width:calc(100% - 20px);
-	float:left;
-	padding:10px;
+.field-input ,.field-select{
+	width:100%;
+	padding:0 1rem;
 	font-family:inherit;
-}		
+	line-height: 4.2rem;
+	height: 4.2rem;
+	border: 0;
+	font-size: 1.4rem;
+	border-bottom: 1px solid #eee;
+	outline:none;
+	}
+.field-input:focus{
+	border-bottom: 1px solid #f60;
+	}
+.field-select{
+	display: flex;
+	flex-direction: row;
+	}
+.form-group select{
+	border: 0;
+	background: none;
+	outline:none;
+	width: 100%;
+	}
+.chang_button{
+	padding: 2rem 1rem;
+	background: #fff;
+	}
+.chang_button input[type=submit]{
+	width: 100%;
+	height: 4.2rem;
+	background: #4CA0FF;
+	border: 0;
+	font-size: 1.6rem;
+	color: #fff;
+	border-radius: .4rem;
+	}
 </style>
 
 <!--必要样式-->
@@ -42,7 +87,7 @@ label{
 </head>
 <body>
 <div class="container">
-	<div class="content">		
+	<div class="content">
 		<form action="{{ url('store') }}" method="post">
 			@if (count($errors) > 0)
 				<div class="alert alert-danger">
@@ -55,55 +100,59 @@ label{
 			@endif
 				<div class="form-group">
 					<label for="city">项目名:</label>
-					<select id="city" name="project_Id" class="city pickout" placeholder="选择项目">
-						@foreach($projects as $project)
-						<option data-icon="&spades;" value="{{ $project->id }}">{{ $project->project_name ? $project->project_name : '暂无' }}</option>
-						@endforeach
-					</select>
+					<div class="field-select">
+						<select name="project_Id" class="city pickout">
+							@foreach($projects as $project)
+							<option data-icon="&spades;" value="{{ $project->id }}">{{ $project->project_name ? $project->project_name : '暂无' }}</option>
+							@endforeach
+						</select>
+					</div>
 				</div>
 
 				{{ csrf_field() }}
 			<div class="form-group">
 				<label for="name">品名:</label>
-				<input type="text" name="name" id="name" class="field-input">
+				<input type="text" name="name" id="name" class="field-input" placeholder="请填写品名">
 			</div>
 				<div class="form-group">
 					<label for="name">数量:</label>
-					<input type="text" name="amount" id="name" class="field-input">
+					<input type="text" name="amount" id="name" class="field-input" placeholder="请填写数量">
 				</div>
 				<div class="form-group">
 					<label for="state">经手人:</label>
-					<select id="state" name="handle_Id" class="state pickout" placeholder="选择经手人" >
-						@foreach($handles as $handle)
-							<option value="{{ $handle->id }}">{{ $handle->handle_name ? $handle->handle_name : '暂无' }}</option>
-						@endforeach
-					</select>
+					<div class="field-select">
+						<select name="handle_Id" class="state pickout" >
+							@foreach($handles as $handle)
+								<option value="{{ $handle->id }}">{{ $handle->handle_name ? $handle->handle_name : '暂无' }}</option>
+							@endforeach
+						</select>
+					</div>
 				</div>
 				<div class="form-group">
 					<label for="name">运价:</label>
-					<input type="text" name="price" value="{{ old('price') }}" id="name" class="field-input">
+					<input type="text" name="price" value="{{ old('price') }}" id="name" class="field-input" placeholder="请填写运价">
 				</div>
 				<div class="form-group">
 					<label for="name">现金支出:</label>
-					<input type="text" name="cash_disburse" value="{{ old('cash_disburse') }}" id="cash_disburse" class="field-input">
+					<input type="text" name="cash_disburse" value="{{ old('cash_disburse') }}" id="cash_disburse" class="field-input" placeholder="请填写现金支出">
 				</div>
 
 				<div class="form-group">
 				<label for="name2">现金回收:</label>
-				<input type="text" name="cash_recover" value="{{ old('cash_recover') }}" id="cash_recover" class="field-input">
+				<input type="text" name="cash_recover" value="{{ old('cash_recover') }}" id="cash_recover" class="field-input" placeholder="请填写现金回收">
 			</div>
 				<div class="form-group">
 					<label for="name2">油卡支出:</label>
-					<input type="text" name="oil_disburse" value="{{ old('oil_disburse') }}" id="cash_recover" class="field-input">
+					<input type="text" name="oil_disburse" value="{{ old('oil_disburse') }}" id="cash_recover" class="field-input" placeholder="请填写油卡支出">
 				</div>
 				<div class="form-group">
 					<label for="name2">油卡回收:</label>
-					<input type="text" name="oil_recover" value="{{ old('oil_recover') }}" id="cash_recover" class="field-input">
+					<input type="text" name="oil_recover" value="{{ old('oil_recover') }}" id="cash_recover" class="field-input" placeholder="请填写油卡回收">
 				</div>
 
 				<div class="form-group">
 					<label for="name2">备注:</label>
-					<input type="text" name="note" value="{{ old('note') }}" id="cash_recover" class="field-input">
+					<input type="text" name="note" value="{{ old('note') }}" id="cash_recover" class="field-input" placeholder="请填写备注">
 				</div>
 				<div class="change_box" href="">
 					<div class="chang_button">
@@ -121,26 +170,7 @@ label{
 	</script>
 	{{--layer.msg('玩命提示中');--}}
 @endif
-<script src="/js/pickout.js"></script>
-<script>
 
-	// Preparar o select
-	//pickout.to('.pickout');
-	pickout.to({
-		el:'.city',
-		theme: 'dark', 
-		search: true
-	});
-
-	pickout.to({
-		el:'.state',
-		theme: 'clean',
-	});
-
-	// Caso o valor já venha do servidor, já atribui a seleção automaticamente
-	pickout.updated('.city');
-	
-</script>
 
 </body>
 </html>
