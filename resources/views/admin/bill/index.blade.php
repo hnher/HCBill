41bf4a2794bffa7a25e330a42be06451c084c1e3
Retr0 <dyy@dyy.name>
2018-05-13 12:45:27 +0800

    Add Bill No

--- resources/views/admin/bill/index.blade.php
+++ resources/views/admin/bill/index.blade.php
@@ -131,7 +131,7 @@
 						<tbody>
 						@foreach($bills as $bill)
 							<tr>
-
+								<td>{{ $bill->no ? $bill->no : '暂无' }}</td>
 								<td>{{ $bill->project->project_name }}</td>
 								<td>{{ $bill->name }}</td>
 								<td class="text-danger">{{ $bill->amount }}</td>
@@ -144,7 +144,7 @@
 								<td>{{ $bill->actual_disburse }}</td>
 								<td>{{ $bill->remaining }}</td>
 								<td>{{ $bill->note ? $bill->note : '无'  }}</td>
-								<td>{{ $bill->customize_time ? date("Y/m/d", $bill->customize_time) : date("Y/m/d", $bill->created_at) }}</td>
+								<td>{{ $bill->customize_time ? date("Y/m/d", $bill->customize_time) : $bill->created_at }}</td>
 								<td>{{ $bill->user ? $bill->user->name : '未知' }}</td>
 								<td class="nscs-table-handle">
 									<span><a href="{{ url('admin/bill/edit',['id'=>$bill->id]) }}" class="btn-blue"><i class="fa fa-edit"></i><p>编辑</p></a></span>
